bd9b1a593a082c00bb32784e07f747fb09399b33
Austin Wood <austin@austinkwood.com>
2016-10-22 12:50:30 +0300

    Update JSON and TO DO

--- Deep Work/ToDo.swift
+++ Deep Work/ToDo.swift
@@ -3,44 +3,46 @@
 //
 // RESUME WITH:
 //
-// Display details:
-//   # Stylize today
-//   # If entry is active:
-//       # Cell color
-//       # Remove stop time from output string
-//       # Update timer each second
+// HistoryVC:
 //   # Swipe to edit
 //       # Delete entry
 //       # Edit note
 //       # Edit start/stop time
 //   # Add edit project button in upper right
-//   # Add summary views to top of VC
-//   # Stylize this week (color code with summary view at top of VC)
+//       # Edit project name
 //
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
 //
 // TO DO:
 //
-// Bug: single tapping in collection view where no cell exists throws error
-// Turn off landscape
-// Automatically capitalize first letter of entry note, and after periods
-// Pause timer when "Great work!" screen appears (resume is 'Continue working' pressed), currently still running in background
-// Animation: When starting timer: fade button color in, fade other buttons text/outline to gray
-// Edge swipe to go back to main VC from HistoryVC
-
-// Visual time line at top (like Hours... No! Better, like my new sketch)
-// Give projects an Area parent / tags
-// Daily/Weekly goals (see Evernote)
-// Sort JSON time entries for readability
-// Delete/archive timers
-// Create an asynchronous request in History, upon completion scroll to bottom
-// Add randomized confimation messages to "Add note"
-// Verify that time entries are disaplayed the same when moving across time zones
-// Animation: Invalid action if trying to start a timer while another is running
-// Replace fatalError with something friendlier
-// Refactor time summations (todayTime and weekTime DRY)
-// Professional design (see Evernote, by asset pack?)
+// Priority 1
+//   # Bug: single tapping in collection view where no cell exists throws error
+//   # Turn off landscape
+//   # Automatically capitalize first letter of entry note, and after periods
+//   # Animation: When starting timer: fade button color in, fade other buttons text/outline to gray
+//   # Edge swipe to go back to main VC from HistoryVC
+//
+// Priority 2
+//   # Visual time line at top (like Hours... No! Better, like my new sketch)
+//   # Give projects an Area parent / tags
+//   # Daily/Weekly goals (see Evernote)
+//   # Sort JSON time entries for readability
+//   # Delete/archive timers (when dragging, views at top can turn into trash can / archive bin)
+//   # Add randomized confimation messages to "Add note"
+//   # Verify that time entries are disaplayed the same when moving across time zones
+//   # Animation: Invalid action if trying to start a timer while another is running
+//   # Replace fatalError with something friendlier
+//   # Refactor time summations (todayTime and weekTime DRY)
+//   # Professional design (see Evernote, by asset pack?)
+//   # HistoryVC:
+//       # Add summary views to top of VC
+//       # Stylize today
+//       # Stylize this week (color code with summary view at top of VC)
+//
+// After v1.0
+//   # Create an asynchronous request in History, upon completion scroll to bottom
+//   # Core Data optimization (see course notes)
 //
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
